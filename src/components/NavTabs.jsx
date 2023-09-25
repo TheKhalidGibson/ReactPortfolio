a0e6096ef1289e033076df5be3b6ae74cc83a612
Khalid G <kgibson4795@gmail.com>
2023-09-25 15:03:03 -0400

    bootstrap navbar and links

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -6,9 +6,10 @@ function NavTabs() {
   const currentPage = useLocation().pathname;
 
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link
+    <>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark text-light px-2">
+<div class="container-fluid">
+<Link
           to="/"
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
@@ -16,9 +17,13 @@ function NavTabs() {
         >
           Home
         </Link>
-      </li>
-      <li className="nav-item">
-        <Link
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+      <Link
           to="/About"
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
@@ -26,8 +31,8 @@ function NavTabs() {
           About
         </Link>
       </li>
-      <li className="nav-item">
-        <Link
+      <li class="nav-item">
+      <Link
           to="/Blog"
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === '/Blog' ? 'nav-link active' : 'nav-link'}
@@ -35,16 +40,27 @@ function NavTabs() {
           Blog
         </Link>
       </li>
-      <li className="nav-item">
-        <Link
+      <li class="nav-item">
+      <Link
           to="/Contact"
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
         >
           Contact
         </Link>
+      
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Full Stack Developer</a>
       </li>
     </ul>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</div>
+</nav></>
   );
 }
 
