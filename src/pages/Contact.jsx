f1411d3ea5c2ae40abc8e8f00696c78db1e986af
Khalid G <kgibson4795@gmail.com>
2023-09-26 09:53:20 -0400

    added contact page inputs and button

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,37 @@
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Me</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+   
+
+
+    
+
+
+    <div class="mt-5 mx-5">
+
+      <p class="text-center mt-2 fs-2">Stay in Touch</p>
+
+        <h5>Name:</h5>
+
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Type Name Here"/>
+      </div>
+
+        <h5>Email:</h5>
+
+      <div class="input-group mb-3"> 
+        <input type="text" class="form-control" placeholder="Type Email Here"/>
+      </div>
+
+        <h5>Message:</h5>
+
+      <div class="input-group">            
+        <textarea class="form-control" placeholder="Type Message Here"></textarea>
+      </div>
+
+
+      <button type="button" class="btn btn-light btn-lg mt-3">Submit</button>
+
     </div>
-  );
+    
+      );
 }
