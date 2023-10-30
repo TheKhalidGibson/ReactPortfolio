ac1520d38849b1531c6c3b8bb88d2711cf39aefd
Khalid G <kgibson4795@gmail.com>
2023-10-30 10:57:01 -0400

    minor changes.. added space in code

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
 import App from './App';
 import Error from './pages/Error';
 import About from './pages/About';
